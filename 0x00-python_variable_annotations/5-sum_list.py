f987da75d93db204d9f2355c1572536cf063d294
mumvekeri <134971766+mumvekeri@users.noreply.github.com>
2024-05-02 18:31:53 +0200

    Create 5-sum_list.py

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+'''Task 5 Answer'''
+from typing import List
+
+
+
+def sum_list(input_list: List[float]) -> float:
+    """
+    Calculate the sum of a list of floats.
+
+    Args:
+        input_list (List[float]): A list of float numbers.
+
+    Returns:
+        float: The sum of all elements in the input_list.
+    """
+    return sum(input_list)
