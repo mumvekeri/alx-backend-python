c7ecf267630439fb1c06b45950572ff2e30fb29c
mumvekeri <134971766+mumvekeri@users.noreply.github.com>
2024-05-02 19:46:12 +0200

    Update 9-element_length.py

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -1,11 +1,19 @@
 #!/usr/bin/env python3
 '''Task 9 answer
 '''
-from typing import Iterable, List, Sequence, Tuple
+from typing import Iterable, List, Tuple, Union
 
 
-def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
-    '''Let's duck type an iterable object
+def element_length(lst: Iterable[Union[str, bytes]]) -> List[Tuple[Union[str, bytes], int]]:
+    '''
+    Generate a list of tuples where each tuple contains an element from the input iterable `lst`
+    along with its length.
+
+    Args:
+        lst (Iterable[Union[str, bytes]]): An iterable of strings or bytes.
+
+    Returns:
+        List[Tuple[Union[str, bytes], int]]: A list of tuples where each tuple contains an element
+                                              from `lst` and its corresponding length.
     '''
     return [(i, len(i)) for i in lst]
-
