afb6941afdf51661f624d1c82e8b3d6d99b2b5b7
mumvekeri <134971766+mumvekeri@users.noreply.github.com>
2024-05-02 18:05:46 +0200

    Create 2-floor.py

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+'''task 2 answer'''
+
+
+import math
+
+def floor(n: float) -> int:
+    """
+    Return the floor of the given float.
+
+    Args:
+        n (float): The input float number.
+
+    Returns:
+        int: The floor value of n.
+    """
+    return math.floor(n)
