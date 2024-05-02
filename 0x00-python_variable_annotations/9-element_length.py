86703decf8193651fbcb16fc04ee383901014ca8
mumvekeri <134971766+mumvekeri@users.noreply.github.com>
2024-05-02 18:54:21 +0200

    Create 9-element_length.py

---
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+''' task 9 answer
+'''
+from typing import List, Tuple
+
+
+def element_length(lst: List[str]) -> List[Tuple[str, int]]:
+    """
+    Return a list of tuples where each tuple contains an element from the input list `lst`
+    along with its length.
+
+    Args:
+        lst (List[str]): A list of strings.
+
+    Returns:
+        List[Tuple[str, int]]: A list of tuples where each tuple contains an element
+                               from `lst` and its corresponding length.
+    """
+    return [(i, len(i)) for i in lst]
